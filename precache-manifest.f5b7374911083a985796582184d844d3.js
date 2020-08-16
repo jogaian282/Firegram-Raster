4acb2bb2a5ad658347e177610da1523a7c4dcb76
KarthikJogaian <karthikjogaianpm@yahoo.in>
2020-08-16 19:44:43 +0530

    Updates

---
+++ precache-manifest.f5b7374911083a985796582184d844d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0f47bd38dd68b47ca7318e6dadd1b7",
+    "url": "/Firegram-Raster/index.html"
+  },
+  {
+    "revision": "7279c39f2eb9c998e4bd",
+    "url": "/Firegram-Raster/static/css/main.4f92c142.chunk.css"
+  },
+  {
+    "revision": "a47bd3bdd9c570cec44a",
+    "url": "/Firegram-Raster/static/js/2.8342d35f.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/Firegram-Raster/static/js/2.8342d35f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7279c39f2eb9c998e4bd",
+    "url": "/Firegram-Raster/static/js/main.ee56831e.chunk.js"
+  },
+  {
+    "revision": "e7a24921d7c779a7eb91",
+    "url": "/Firegram-Raster/static/js/runtime-main.8d7861e5.js"
+  }
+]);
